b2fab8e8dccd31118f50d01d915750092dee6318
eurodiego <eurodiego@yahoo.com>
2024-05-15 18:32:40 -0300

    Arreglos y mejoras en la validación al registrar. Agregué imagen de fondo

--- static/js/validation-register.js
+++ static/js/validation-register.js
@@ -1,30 +1,58 @@
 let btnSend = document.querySelector("#btn-send");
 
-btnSend.addEventListener('click',function(){
+btnSend.addEventListener('click',function(e) {
+    e.preventDefault();
     let firstname = document.querySelector("#firstname");
     let lastname = document.querySelector("#lastname");
     let email = document.querySelector("#email");
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     let birthdate = document.querySelector("#birthdate");
     let password = document.querySelector("#password");
+    let exito = true;
 
-    if(firstname.value.trim() == '' || firstname.value.trim().length < 3 ) {
+    if(firstname.value.trim() == '' || firstname.value.trim().length < 2 ) {
         document.querySelector("#error-firstname").innerHTML = "Debe completar el campo Nombre";
+        exito = false;
+    } else {
+        document.querySelector("#error-firstname").innerHTML = "";
     }
 
-    if(lastname.value.trim() == '') {
+    if(lastname.value.trim() == '' || lastname.value.trim().length < 2 ) {
         document.querySelector("#error-lastname").innerHTML = "Debe completar el campo Apellido";
+        exito = false;
+    } else {
+        document.querySelector("#error-lastname").innerHTML = "";
     }
 
     if(!emailRegex.test(email.value)) {
         document.querySelector("#error-email").innerHTML = "El email no tiene un formato valido.";
+        exito = false;
+    } else {
+        document.querySelector("#error-email").innerHTML = "";
     }
 
     if(password.value.trim() == '') {
         document.querySelector("#error-password").innerHTML = "Debe completar el campo Contraseña";
+        exito = false;
+    } else {
+        document.querySelector("#error-password").innerHTML = "";
     }
 
     if(birthdate.value.trim() == '') {
-        document.querySelector("#error-birthdate").innerHTML = "Debe completar el campo Fecha de nacimiento";
+        document.querySelector("#error-birthdate").innerHTML = "Debe completar este campo";
+        exito = false;
+    } else {
+        document.querySelector("#error-birthdate").innerHTML = "";
+    }
+
+    if(exito) {
+        setTimeout(succ, 1500);
+        document.querySelector("#btn-send").innerHTML = "Registrando  <i class='fas fa-spinner fa-spin'></i>"
     }
 });
+
+function succ() {
+    document.querySelector("#success").innerHTML = "Datos enviados con éxito!";
+    document.querySelector("#btn-send").innerHTML = "Registrado"
+    document.querySelector("#btn-send").disabled = true;
+}
